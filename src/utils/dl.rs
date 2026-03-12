5c092307b03513b9f277fada4666ca02bbc7c720
tox <tox@tox.wtf>
2026-03-11 19:51:25 -0500

    feat: cleanup dl logic

--- src/utils/dl.rs
+++ src/utils/dl.rs
@@ -12,11 +12,10 @@ use std::{
         Path,
         PathBuf,
     }, process::exit, str::FromStr, sync::{
-        Arc,
-        atomic::{
+        Arc, LazyLock, atomic::{
             AtomicBool,
             Ordering,
-        },
+        }
     }, time::{
         Duration,
         SystemTime,
@@ -44,18 +43,20 @@ use tokio::task;
 
 use crate::unravel;
 
+
+
 // TODO: Documentation
 // NOTE: Beware the distinction between timeout and connect_timeout
 //
 /// # Creates a reqwest client
 ///
-/// This client follows up to 16 redirects and has a timeout of 32 seconds. It also sets the user
-/// agent to crate/version.
+/// This client follows up to 32 redirects and has a connection timeout of 120 seconds. It also
+/// sets the user agent to crate/version.
 #[allow(clippy::expect_used)]
-fn create_client() -> Result<Client, reqwest::Error> {
+static CLIENT: LazyLock<Client> = LazyLock::new(|| {
     let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     Client::builder()
-        .redirect(Policy::limited(16))
+        .redirect(Policy::limited(32))
         .default_headers({
             let mut headers = HeaderMap::new();
             headers.insert(
@@ -64,9 +65,10 @@ fn create_client() -> Result<Client, reqwest::Error> {
             );
             headers
         })
-        .connect_timeout(Duration::from_secs(32))
+        .connect_timeout(Duration::from_mins(2))
         .build()
-}
+        .expect("Failed to build client")
+});
 
 #[derive(Debug)]
 pub struct Download {
@@ -124,7 +126,6 @@ fn get_local_modtime(path: &Path) -> Option<SystemTime> {
 }
 
 async fn download_file<P: AsRef<Path>>(
-    client: Client,
     url: &str,
     file_path: P,
     download_extant: bool,
@@ -133,18 +134,18 @@ async fn download_file<P: AsRef<Path>>(
 
     // Fetch the url
     debug!("Fetching '{url}'");
-    let resp = client
+    let resp = CLIENT
         .get(url)
         .send()
         .await?
         .error_for_status()?;
 
-    // Skip extant files, but only if upstream's modtime is greater than local
+    // Skip extant files, but only if upstream's modtime is less than or equal to local
     if file_path.exists() && !download_extant {
         let upstream_modtime = get_upstream_modtime(resp.headers()).unwrap_or_else(SystemTime::now);
         let local_modtime = get_local_modtime(file_path).unwrap_or(SystemTime::UNIX_EPOCH);
 
-        if upstream_modtime < local_modtime {
+        if upstream_modtime <= local_modtime {
             debug!(
                 "Skipping download for extant file '{}'",
                 file_path.display()
@@ -192,27 +193,17 @@ pub async fn download_sources<P: AsRef<Path>, Q: AsRef<Path>>(
     mkdir_p(&sources_dir)?;
 
     let failed = Arc::new(AtomicBool::new(false));
-    let client = match create_client() {
-        | Ok(c) => c,
-        | Err(ref e) => {
-            error!("Failed to create reqwest client: {e}");
-            error!("Unable to download sources :(");
-            unravel!(e);
-            exit(1)
-        },
-    };
 
     let dls = read_dls_from_file(sources_list)?;
     trace!("Here's what dls looks like:\n {dls:#?}");
     let mut tasks = Vec::new();
 
     for dl in dls {
-        let client = client.clone();
         let failed = Arc::clone(&failed);
         let dest = sources_dir.as_ref().join(&dl.dest);
 
         let task = task::spawn(async move {
-            if let Err(e) = download_file(client, &dl.url, &dest, download_extant)
+            if let Err(e) = download_file(&dl.url, &dest, download_extant)
                 .await
                 .permit(|e| matches!(e, DownloadError::Extant(_)))
             {
