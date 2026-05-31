46de990854d363434354d49c6b2df36c0383b43a
tox <tox@tox.wtf>
2026-05-30 22:21:00 -0500

    !feat: remove log file; better logs

--- src/utils/init.rs
+++ src/utils/init.rs
@@ -1,24 +1,16 @@
 // utils/init.rs
 //! Initialization utilities
 
-use std::path::Path;
+use std::io;
 use std::process::exit;
 use std::str::FromStr;
-use std::sync::OnceLock;
 use std::time::Instant;
-use std::{fs, io};
 
 use tracing::metadata::LevelFilter;
-use tracing_appender::non_blocking::WorkerGuard;
-use tracing_appender::rolling;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::time::FormatTime;
-use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 use crate::config::CONFIG;
-
-static LOG_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
-const LOG_FILE: &str = "/var/log/lfstage/lfstage.log";
 
 pub fn init() {
     check_perms();
@@ -40,6 +32,7 @@ struct Uptime(Instant);
 impl Uptime {
     /// # Create a new [`Uptime`]
     #[inline]
+    #[must_use]
     fn new() -> Self { Self(Instant::now()) }
 }
 
@@ -55,13 +48,6 @@ impl FormatTime for Uptime {
 
 #[allow(clippy::expect_used, clippy::unwrap_used)]
 fn log() {
-    if let Some(parent) = Path::new(LOG_FILE).parent()
-        && !parent.exists()
-    {
-        fs::create_dir_all(parent).expect("Failed to create directory");
-    }
-    fs::write(LOG_FILE, "").expect("Failed to truncate log file");
-
     let debug = cfg!(debug_assertions);
     let level = LevelFilter::from_str(&CONFIG.log_level).unwrap_or(match debug {
         | true => LevelFilter::TRACE,
@@ -70,19 +56,14 @@ fn log() {
 
     let filter = EnvFilter::new(format!("{level},rustls=warn,hyper_util=warn,reqwest=warn"));
 
-    let (dir, file) = LOG_FILE.rsplit_once('/').unwrap_or((".", LOG_FILE));
-    let file_appender = rolling::never(dir, file);
-    let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
-
     tracing_subscriber::fmt()
         .with_env_filter(filter)
         .with_level(true)
         .with_target(debug)
         .with_line_number(debug)
         .with_timer(Uptime::new())
-        .with_writer(file_writer.and(io::stdout))
+        .with_writer(io::stderr)
+        .with_ansi_sanitization(false)
         .compact()
         .init();
-
-    LOG_GUARD.set(guard).expect("logs were inited more than once");
 }
